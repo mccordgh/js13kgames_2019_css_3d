89aa3e6ed5f1d63405d92888c9dd5c5aa58863ab
Matthew McCord <mattm.eternal@gmail.com>
2019-08-17 20:10:46 -0700

    some kind of rotation with skipping bad rotations

--- src/index.js
+++ src/index.js
@@ -88,18 +88,20 @@ function handleKeyUp(event) {
         39() {
             // Rotate right
 
-            parent.rotations.x -= 90;
-            parent.rotations.y -= 45;
-            parent.rotations.z += 45;
+            parent.rotations.x -= twoRotate();
+            parent.rotations.y -= oneRotate();
+            parent.rotations.z += oneRotate();
             parent.style.transform = `rotateX(${parent.rotations.x}deg) rotateY(${parent.rotations.y}deg) rotateZ(${parent.rotations.z}deg)`;
+            rotations += 1;
         },
         37() {
             // Rotate left
 
-            parent.rotations.x += 90;
-            parent.rotations.y += 45;
-            parent.rotations.z -= 45;
+            parent.rotations.x += twoRotate();
+            parent.rotations.y += oneRotate();
+            parent.rotations.z -= oneRotate();
             parent.style.transform = `rotateX(${parent.rotations.x}deg) rotateY(${parent.rotations.y}deg) rotateZ(${parent.rotations.z}deg)`;
+            rotations -= 1;
         }
     }
 
@@ -109,7 +111,18 @@ function handleKeyUp(event) {
 
     parent.style.transform = `rotateX(${parent.rotations.x}deg) rotateY(${parent.rotations.y}deg) rotateZ(${parent.rotations.z}deg)`;
 
-    // console.log(`X(${parent.rotations.x}) Y(${parent.rotations.y}) Z(${parent.rotations.z})`)
+    rotationSkip = (rotations - 1) % 4 == 0;
+    console.log(rotationSkip, rotations)
+    // console.log(`X(${parent.rotations.x % 45}) Y(${parent.rotations.y % 45}) Z(${parent.rotations.z % 45})`)
+    // console.log({rotations: (rotations - 2 ) % 4})
+}
+
+function oneRotate() {
+    return  rotationSkip ? rotationAmt * 2 : rotationAmt;
+}
+
+function twoRotate() {
+    return rotationSkip ? rotationAmt * 4 : rotationAmt * 2;
 }
 
 function setupListeners() {
@@ -118,3 +131,6 @@ function setupListeners() {
 
 setupListeners();
 var cubes = makeCubes(1);
+var rotations = 0;
+var rotationAmt = 45;
+var rotationSkip = false;
